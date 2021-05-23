88efd695607602d370568fe56d350359ee7272a2
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-05-23 22:23:03 +0700

    Bai 07: Hiển thị bằng Razor

---
+++ WebApplication2/Models/CategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+  public class CategoryModel
+  {
+    private OnlineDbContext context = null;
+    public CategoryModel()
+    {
+      context = new OnlineDbContext();
+    }
+    public List<Category> GetAllCategory()
+    {
+      var list = context.Database.SqlQuery<Category>("GetAllCategory").ToList();
+      return list;
+    }
+  }
+}
---
+++ WebApplication2/WebApplication2/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Areas.Admin.Controllers
+{
+  public class CategoryController : Controller
+  {
+    // GET: Admin/Category
+    public ActionResult Index()
+    {
+      var iplCategory = new CategoryModel();
+      var model = iplCategory.GetAllCategory();
+      return View(model);
+    }
+  }
+}
