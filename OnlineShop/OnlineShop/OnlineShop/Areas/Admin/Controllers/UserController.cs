ceba58deca318d51c3c3bb2f7a43874453406ee1
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-06-06 16:46:57 +0700

    Bai17: Insert a user to database

---
+++ OnlineShop/OnlineShop/OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+using OnlineShop.Common;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+      public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var pass = Encryptor.MD5Hash(user.Password);
+                user.Password = pass;
+                long id = dao.Insert(user);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Add new user failed!");
+                }
+            }
+            return View();
+        }
+    }
+}
