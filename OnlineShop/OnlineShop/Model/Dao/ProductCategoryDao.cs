c169ba62cf72a099495827422a5f431139ce261b
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-07-06 18:29:19 +0700

    [OnlineShop] Create/Delete ProductCategory

--- OnlineShop/OnlineShop/Model/Dao/ProductCategoryDao.cs
+++ OnlineShop/OnlineShop/Model/Dao/ProductCategoryDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,31 @@ namespace Model.Dao
     public List<ProductCategory> GetAllCategoryActive()
     {
       return db.ProductCategories.Where(x => x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
+    }
+    public IEnumerable<ProductCategory> GetAllCategory(int page, int pagesize)
+    {
+      return db.ProductCategories.OrderBy(x => x.DisplayOrder).ToPagedList(page, pagesize);
+    }
+    public long Insert(ProductCategory data)
+    {
+      if (db.ProductCategories.SingleOrDefault(x => x.Name == data.Name) == null)
+      {
+        db.ProductCategories.Add(data);
+        db.SaveChanges();
+        return data.ID;
+      }
+      else return 0;
+    }
+    public bool Delete(long id)
+    {
+      if (db.ProductCategories.SingleOrDefault(x => x.ID == id) != null)
+      {
+        var data = db.ProductCategories.Find(id);
+        db.ProductCategories.Remove(data);
+        db.SaveChanges();
+        return true;
+      }
+      else return false;
     }
   }
 }
---
+++ OnlineShop/OnlineShop/OnlineShop/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,67 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+  public class ProductCategoryController : BaseController
+  {
+    // GET: Admin/ProductCategory
+    public ActionResult Index(int pagedList = 1, int pagesize = 10)
+    {
+      var dao = new ProductCategoryDao();
+      var list = dao.GetAllCategory(pagedList, pagesize);
+      return View(list);
+    }
+    [HttpGet]
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(ProductCategory data)
+    {
+      if (ModelState.IsValid)
+      {
+        var dao = new ProductCategoryDao();
+        data.CreatedBy = ((UserLogin)Session[CommonConstant.USER_SESSION]).UserName;
+        data.CreatedDate = DateTime.Now;
+        long id = dao.Insert(data);
+        if (id > 0)
+        {
+          SetAlert("Add user success!", AlertType.SUCCESS);
+          return RedirectToAction("Index", "ProductCategory");
+        }
+        else if (id == 0)
+        {
+          SetAlert("Add user fail!", AlertType.ERROR);
+          ModelState.AddModelError("", "ProductCategory is exist!");
+        }
+        else
+        {
+          SetAlert("Add user fail!", AlertType.ERROR);
+          ModelState.AddModelError("", "Add new user failed!");
+        }
+      }
+      return View();
+    }
+    public ActionResult Delete(long id) {
+      var dao = new ProductCategoryDao();
+      bool ret = dao.Delete(id);
+      if (ret)
+      {
+        return RedirectToAction("Index", "ProductCategory");
+      }
+      else
+      {
+        ModelState.AddModelError("", "Delete ProductCategory failed!");
+      }
+      return View();
+    }
+  }
+}
