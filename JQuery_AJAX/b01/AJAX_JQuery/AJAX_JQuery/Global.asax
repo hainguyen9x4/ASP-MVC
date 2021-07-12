7aca4bc25ae5639637d06d102a2e1cd491187b34
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-07-12 22:06:04 +0700

    [JQuery Ajax] Bai 1,2: AJAX: .load(); $.get()

---
+++ JQuery_AJAX/b01/AJAX_JQuery/AJAX_JQuery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AJAX_JQuery.MvcApplication" Language="C#" %>
---
+++ JQuery_AJAX/b01/AJAX_JQuery/AJAX_JQuery/file.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" content="text/html" />
+    <title>PHP file</title>
+</head>
+<body>
+    
+    <h1><?php echo 'Hello world'; ?></h1>
+</body>
+</html>
