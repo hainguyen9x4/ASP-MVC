eec416aae30a3e1b17940f551428d70062334605
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-07-20 16:01:11 +0700

    [OnlineShop]Update data for bool? Add slide effect

---
+++ OnlineShop/OnlineShop/OnlineShop/Areas/Admin/Controllers/SlideController.cs
@@ -0,0 +1,34 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+  public class SlideController : Controller
+  {
+    // GET: Admin/Slide
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [HttpGet]
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create([Bind(Include = "DisplayOrder,Description,Status")]Slide slide, HttpPostedFileBase  Image)
+    {
+      if (ModelState.IsValid)
+      {
+        var dao = new SlideDao();
+        
+      }
+      return View();
+    }
+  }
+}
