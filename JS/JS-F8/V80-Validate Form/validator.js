e72950791fb394fc1082931a91c9259d2c218a33
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-08-18 15:58:35 +0700

    [JS] submit form: thêm submit bằng javascript lấy data từ form -Có thể submit bằng form nếu ko truyền tham số: onSubmit trong hàm Validator()

--- JS/JS-F8/V80-Validate Form/validator.js
+++ JS/JS-F8/V80-Validate Form/validator.js
@@ -1,14 +1,12 @@
 
-var selectorRules ={};// 
-
 function Validator(option) {
+    var selectorRules ={};// 
 /*
 Validate the input element
+-Ko co loi tra ra là true, co lói tra ra la false
 */
     function validateInput(elementInput,rule,elementMsg){
         var mes;
-        //console.log("mes in validateInput func: "+mes);
-        
         var rules = selectorRules[rule.selector];//lấy các rule của 1 slector cụ thể
         for(var i=0 ;i< rules.length;i++){// duyệt qua các rule của selector
             mes = rules[i](elementInput.value);// thực hiện rule và trả về message
@@ -18,20 +16,54 @@ Validate the input element
             if(elementMsg){
                 elementMsg.innerText = mes;
                 elementMsg.classList.add('invalid');
+                isError = false;
             }
         }
+        return !mes;
     }
 
     var elementForm = document.querySelector(option.form);
 
     if(elementForm){
+        //xu ly khi nhan nut resgiter
+        elementForm.onsubmit = function(e){
+            e.preventDefault();
+            var isFormValid = true;
+            //lap qua từng rule và gọi validate, nếu có lỗi thì báo lỗi và báo form is invalid.
+            option.rules.forEach(rule => {
+                var inputElement = elementForm.querySelector(rule.selector);
+                var elementMsg = inputElement.parentElement.querySelector(option.selectorMesError);
+                if(!validateInput(inputElement,rule,elementMsg))
+                {
+                    isFormValid = false; 
+                }
+            });
+            //neu form valid thi thuc hien lay data form
+            if(isFormValid){
+                //submit với javaScript
+                if(typeof option.onSubmit === 'function'){
+                var enableInputs = elementForm.querySelectorAll('[name]:not([disable])');//ko lay thang disable
+                //duyet tat ca để đưa vào object là 1 mảng các values
+                var formValues = Array.from(enableInputs).reduce(function(values,item){
+                    return (values[item.name]=item.value) && values;
+                },{});
+                option.onSubmit(formValues);
+            }
+            //Submit mặc định với onsubmit của form
+            else{
+                elementForm.onsubmit();
+            }
+        };
+        //Lap qua cac rule để gán event cho mỗi rule
         option.rules.forEach(function(rule) {
             
+            //Tạo mảng rule vì 1 selector có thể chạy nhieu rules: rule required, rule là email, chiều dài kí tự
             if(Array.isArray(selectorRules[rule.selector])){
                 selectorRules[rule.selector].push(rule.test);// lan tiep theo them vao mang
             }else{
                 selectorRules[rule.selector] =[rule.test]//lan dau tien chua là mảng, se khoi tao mang
             }
+
             var inputElement = elementForm.querySelector(rule.selector);
             var elementMsg = inputElement.parentElement.querySelector(option.selectorMesError);
             inputElement.onblur = function(){
@@ -72,7 +104,7 @@ Validator.validateLength = function(selector, min, max, mesError){
         return {
             selector: selector,
             test: function(value){
-                return value >= 6 && value < 8 ? undefined : mesError||`Length must be ${min}-${max} characters!`
+                return value.length >= 6 && value.length <= 8 ? undefined : mesError||`Length must be ${min}-${max} characters!`
             }
         };
 }
@@ -82,5 +114,11 @@ Validator.isConfirm = function(selector, getData, mesError){
         test: function(value){
             return value === getData() ? undefined : mesError||'Confirm data is not corrected!';
         }
+    };
+}
+Validator.onCommit = function(selector, getData){
+    return {
+        selector: selector,
+        execute: getData
     };
 }
